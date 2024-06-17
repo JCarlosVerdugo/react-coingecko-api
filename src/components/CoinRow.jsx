fa40baa0b736854a7a9eb5f1a133b0546c81bf31
Carlos Verdugo <carlos@adroitoverseas.com>
2024-06-17 11:13:28 -0700

    Adding tabla of cryptos

---
+++ src/components/CoinRow.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const CoinRow = ({ coin, index }) => {
+
+    console.log(coin)
+
+    return (
+        <tr>
+            <td>{ index }</td>
+            <td>
+                <img src={coin.image} alt={coin.name} style={{ width: '3%'}} className="img-fluid me-4"/>
+                <span> { coin.name } </span>
+                <span className='ms-3 text-secondary text-uppercase'> { coin.symbol } </span>
+            </td>
+            <td>$ { coin.current_price }</td>
+            {/* <td>{ coin.price_change }</td> */}
+            <td>{ coin.price_change_percentage_24h }</td>
+            <td>{ coin.total_volume }</td>
+        </tr>
+    )
+}
+
+export default CoinRow
